17beeeea3965b76e728a8a0b1697cf1df1e11071
Adacy78 <adacy78@gmail.com>
2025-09-16 16:52:32 +0200

    Corriger l'enregistrement du groupe (tabs)

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -29,6 +29,7 @@ export default function RootLayout() {
   return (
     <>
       <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="(tabs)" />
         <Stack.Screen name="auth" />
         <Stack.Screen name="+not-found" />
       </Stack>
